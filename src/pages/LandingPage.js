1f4918a543f5b5b97953e761abc6d7691a360b2d
Adi Nugroho <addieze@ymail.com>
2022-02-20 19:25:29 +0700

    fix most-picked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
+import landingPageData from "json/landingPage.json";
 
 export default function LandingPage(props) {
-  return <Header {...props} />;
+  const refMostPicked = useRef();
+
+  return (
+    <>
+      <Header {...props} />
+      <Hero refMostPicked={refMostPicked} data={landingPageData.hero} />
+      <MostPicked
+        refMostPicked={refMostPicked}
+        data={landingPageData.mostPicked}
+      />
+    </>
+  );
 }
